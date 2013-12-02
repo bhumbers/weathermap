9f0ed4cdad411f792da00514e49e08abcca3d215
felherc <feh17@pitt.edu>
2013-12-02 09:55:22 -0500

    LR multiple variable script
    This script takes a file with all the regression data (values for the
    previous time step on the first 10 columns and for the current one in
    the second 10) and performs the n-degree regression on each of the
    variables

---
+++ src/linReg/nldas2script.m
@@ -0,0 +1,19 @@
+function [x] = nldas2script(degree, trials)
+
+	load dinLinReg.txt;
+	XA = dinLinReg;
+	X = XA(:, 1:10);
+	for var = 1:10
+		y = XA(:, (10 + var));
+		for trial = 1:trials
+			[w(:, trial), Xpoly, terms, tre(1, trial), tse(1, trial)] = polyReg(X, y, degree, 0.1, true);
+		end
+		treavg = mean(tre)
+		tseavg = mean(tse)
+		wavg(:, var) = mean(w, 2);
+	end
+	save terms.txt terms -text;
+	save results.txt wavg -ascii;
+	x = "Done!";
+	
+end
