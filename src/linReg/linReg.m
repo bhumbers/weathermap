70bfd79b90323d0193393d34024c5ddb24dab2b5
bhumbers <raichuman@gmail.com>
2013-12-07 17:15:16 -0500

    LR script tweaks & cleanup

--- src/linReg/linReg.m
+++ src/linReg/linReg.m
@@ -3,6 +3,7 @@
 % @param y the vector with the labels of the examples of size n
 % @return w the vector with the regression coefficients of size m 
 function [w] = linReg(X, y)
-	w = ((y'*X)/(X'*X))';
+% 	w = ((y'*X)/(X'*X))';
+    w = X \ y;
 	%w = pinv(X)*y;
 end
--- src/linReg/nldas2script.m
+++ src/linReg/nldas2script.m
@@ -1,19 +1,50 @@
 function [x] = nldas2script(degree, trials)
 
+    if nargin < 1
+        degree = 1;
+    end
+    
+    if nargin < 2
+        trials = 1;
+    end
+
 	load dinLinReg.txt;
 	XA = dinLinReg;
-	X = XA(:, 1:10);
-	for var = 1:10
-		y = XA(:, (10 + var));
+    
+    numTimesteps = 720;
+    
+    %Cut out so we're looking at data for just one cell
+    XA = XA(1:numTimesteps, :);
+    
+    totVars = size(XA,2) / 2;
+    
+    %Specify variables of interest
+    xVars = 9;
+    yVars = 9;
+    
+	X = XA(:, xVars);
+    Y = XA(:,(totVars+1):end);
+    
+    stdY = std(Y);
+    
+    treavg = zeros(1, totVars);
+    tseavg = zeros(1, totVars);
+    
+	for yVar = yVars
+		y = Y(:, yVar);
 		for trial = 1:trials
-			[w(:, trial), Xpoly, terms, tre(1, trial), tse(1, trial)] = polyReg(X, y, degree, 0.1, true);
+			[w(:, trial), Xpoly, terms, tre(trial), tse(trial)] = polyReg(X, y, degree, 0, true);
 		end
-		treavg = mean(tre)
-		tseavg = mean(tse)
-		wavg(:, var) = mean(w, 2);
+		treavg(yVar) = mean(tre');
+		tseavg(yVar) = mean(tse');
+		wavg(:, yVar) = mean(w, 2);
 	end
 % 	save terms.txt terms -text;
 % 	save results.txt wavg -ascii;
+
+    tseavg = tseavg ./ stdY;
+    tseavg
+
 	x = 'Done!';
 	
 end
